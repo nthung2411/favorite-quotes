5c18d7d044b71a7d5319948124bf8b025f402345
Robert Hung Nguyen <nthung2411@gmail.com>
2017-04-30 15:20:03 +0700

    apply event for button Favorite
    - use Alert to confirm quote selection.

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavParams } from 'ionic-angular';
+import { IonicPage, NavParams, AlertController } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 
 @IonicPage()
@@ -14,9 +14,31 @@ export class QuotesPage implements OnInit {
       this.quoteGroup = this.navParams.data;
     }
 
-  constructor(private navParams: NavParams){  }
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController){  }
 
-  onAddToFavorite(quote: Quote){
-
+  onAddToFavorite(selectedQuote: Quote){
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure ?',
+      message: 'Are you sure you want to add the quote ?',
+      buttons: [
+        {
+          text: 'Yes, go ahead.',
+          handler: ()=> {
+            console.log('OK');
+          }
+        },
+        {
+          text: 'No, I changed my mind!',
+          role: 'cancel',
+          handler: ()=>{
+            console.log('Canceled');
+          }
+        }
+      ]
+    });
+    alert.present();    
   }
 }
