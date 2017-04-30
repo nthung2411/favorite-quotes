2d5e93d47470d5a9a9bbb51b3d7dda7f99e3c977
Robert Hung Nguyen <nthung2411@gmail.com>
2017-04-30 15:21:34 +0700

    Publish new version to github
    - finish tuning Quote UI
    -- use Card to display each quote
    - promt for quote selection
    -- use Alert for prompting

--- docs/build/3.main.js
+++ docs/build/3.main.js
@@ -1,6 +1,6 @@
 webpackJsonp([3],{
 
-/***/ 265:
+/***/ 266:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- docs/build/4.main.js
+++ docs/build/4.main.js
@@ -1,6 +1,6 @@
 webpackJsonp([4],{
 
-/***/ 264:
+/***/ 265:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
